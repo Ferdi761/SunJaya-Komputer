ff486f3cd2f642e46e87c050f5bb1261f2ad5494
Ferytom <garuda.bangkit@gmail.com>
2022-10-19 21:08:51 +0700

    delete all

--- frontend/src/components/admin-pages/MyDataGrid.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/system'
-import { Datagrid } from 'react-admin'
-
-export const MyDatagrid = styled(Datagrid)({
-  backgroundColor: 'Lavender',
-  '& .RaDatagrid-headerCell': {
-    backgroundColor: 'MistyRose',
-  },
-})
