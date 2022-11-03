e0aeeaa8414121a4d1f36d17e5180d6586230358
Hans Sean Nathanael <hans.sean22@gmail.com>
2022-11-03 20:08:29 +0700

    Add ID on pesanan saya + empty cart

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -30,58 +30,74 @@ const Cart = () => {
 
           <div className='flex flex-row gap-5'>
             <div className='w-7/12'>
-              {cartItems.map((item) => (
-                <CartDetail key={item.id} {...item} />
-              ))}
+              {cartQuantity > 0 
+                ? cartItems.map((item) => (
+                  <CartDetail key={item.id} {...item} />
+                ))
+                : <div className='flex justify-center items-start bg-transparent text-black p-0'>
+                  <p className='text-2xl mb-3'>- Keranjang Kosong -</p>
+                </div>
+              }
             </div>
             <div className='w-5/12'>
               <div className='p-10 text-white bg-dark rounded-xl'>
                 <p className='text-2xl mb-3'>Rincian Keranjang</p>
-                <ul>
-                  <li className='flex justify-between items-start bg-transparent text-white p-0'>
-                    <p>Total Barang</p>
-                    <p className='font-semibold'>
-                      {cartQuantity} Barang
-                    </p>
-                  </li>
-                  <li className='flex justify-between items-start bg-transparent text-white p-0'>
-                    <p>Total Berat</p>
-                    <p className='font-semibold'>1 Kg</p>
-                  </li>
-                  <li className='flex justify-between items-start bg-transparent text-white p-0'>
-                    <p>Total Belanja</p>
-                    <p className='font-semibold'>
-                      {formatCurrency(
-                        cartItems.reduce((total, cartItem) => {
-                          const item = storeItems.find(
-                            (i) => i.id === cartItem.id
-                          )
-                          return (
-                            total +
-                            (item?.price || 0) * cartItem.quantity
-                          )
-                        }, 0)
-                      )}
-                    </p>
-                  </li>
-                </ul>
-                <form className='my-4'>
-                  <label className='block text-md mb-2 font-semibold'>
-                    Alamat
-                  </label>
-                  <textarea
-                    className='focus:ring-2 focus:ring-black focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-5 ring-1 ring-slate-200 shadow-sm h-36'
-                    aria-label='Alamat'
-                    placeholder='*Tulis secara lengkap, kecamatan, kota, dan provinsi'
-                    // value={akun.nama}
-                    // onChange={(e) =>
-                    //   setAkun({ ...akun, nama: e.target.value })
-                    // }
-                  />
-                </form>
-                <button className='bg-blue-700 hover:bg-blue-900 w-full rounded-xl font-bold text-2xl py-1 mt-10'>
-                  Pesan
-                </button>
+                {cartQuantity > 0 
+                  ? <>
+                    <ul>
+                      <li className='flex justify-between items-start bg-transparent text-white p-0'>
+                        <p>Total Barang</p>
+                        <p className='font-semibold'>
+                          {cartQuantity} Barang
+                        </p>
+                      </li>
+                      <li className='flex justify-between items-start bg-transparent text-white p-0'>
+                        <p>Total Berat</p>
+                        <p className='font-semibold'>1 Kg</p>
+                      </li>
+                      <li className='flex justify-between items-start bg-transparent text-white p-0'>
+                        <p>Total Belanja</p>
+                        <p className='font-semibold'>
+                          {formatCurrency(
+                            cartItems.reduce((total, cartItem) => {
+                              const item = storeItems.find(
+                                (i) => i.id === cartItem.id
+                              )
+                              return (
+                                total +
+                                (item?.price || 0) * cartItem.quantity
+                              )
+                            }, 0)
+                          )}
+                        </p>
+                      </li>
+                    </ul>
+                    <form className='my-4'>
+                      <label className='block text-md mb-2 font-semibold'>
+                        Alamat
+                      </label>
+                      <textarea
+                        className='focus:ring-2 focus:ring-black focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-5 ring-1 ring-slate-200 shadow-sm h-36'
+                        aria-label='Alamat'
+                        placeholder='*Tulis secara lengkap, kecamatan, kota, dan provinsi'
+                        // value={akun.nama}
+                        // onChange={(e) =>
+                        //   setAkun({ ...akun, nama: e.target.value })
+                        // }
+                      />
+                    </form>
+                    <button className='bg-blue-700 hover:bg-blue-900 w-full rounded-xl font-bold text-2xl py-1 mt-10'>
+                      Pesan
+                    </button>
+                  </>
+                  : <>
+                    <ul>
+                      <li className='flex justify-center items-start bg-transparent text-white p-0'>
+                        <p>- Keranjang Kosong</p>
+                      </li>
+                    </ul>
+                  </>
+                }
               </div>
             </div>
           </div>
