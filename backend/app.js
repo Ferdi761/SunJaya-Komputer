b6e5ce717f22f0e90249ec7ba78986aa83eb0178
Ferytom <garuda.bangkit@gmail.com>
2022-12-12 15:07:48 +0700

    update route

--- backend/app.js
+++ backend/app.js
@@ -1,97 +1,89 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const cors = require("cors");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const multer = require("multer");
-const Sequelize = require("sequelize");
-const { Server } = require('socket.io');
-const port = process.env.PORT || 8000;
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const cors = require('cors')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const multer = require('multer')
+const Sequelize = require('sequelize')
+const { Server } = require('socket.io')
+const port = process.env.PORT || 8000
 
-const { ChatSocketController } = require("./controllers/ChatSocketController");
+// const { ChatSocketController } = require("./controllers/ChatSocketController");
 
-const socketIO = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-});
-const chatSocketController = new ChatSocketController(socketIO);
+// const socketIO = new Server(server, {
+//     cors: {
+//         origin: "*"
+//     }
+// });
+// const chatSocketController = new ChatSocketController(socketIO);
 
 // middleware
-app.use(cookieParser());
+app.use(cookieParser())
 
 // setting up express router
-const routerSignup = require("./routes/routeSignup");
-const routerLogin = require("./routes/routeLogin");
-const routerBarang = require("./routes/routeBarang");
-const routerLogout = require("./routes/routeLogout");
-const routerJenisBarang = require("./routes/routeJenisBarang");
-const routerRekening = require("./routes/routeRekening");
-const routerKeranjang = require("./routes/routeKeranjang");
-const routerPemesanan = require("./routes/routePemesanan");
+const routerSignup = require('./routes/routeSignup')
+const routerLogin = require('./routes/routeLogin')
+const routerBarang = require('./routes/routeBarang')
+const routerLogout = require('./routes/routeLogout')
+const routerJenisBarang = require('./routes/routeJenisBarang')
+const routerRekening = require('./routes/routeRekening')
+const routerKeranjang = require('./routes/routeKeranjang')
+const routerPemesanan = require('./routes/routePemesanan')
 // test all akun
-const routerAkun = require("./routes/routeAkun");
+const routerAkun = require('./routes/routeAkun')
 
 // use dotenv
-require("dotenv").config();
+require('dotenv').config()
 
 // using cors middleware
 // let corsOptions = { origin: "http://localhost:8000" };
 // app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors())
 
 // postgres db Connection
-const { 
-    database,
-    username,
-    password,
-    host,
-    dialect
- } = require("./database/config/config.json").development;
-const sequelize = new Sequelize(
-    database,
-    username,
-    password,
-    { host, dialect }
-);
+const { database, username, password, host, dialect } =
+  require('./database/config/config.json').development
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  dialect,
+})
 
-app.use(express.json());
+app.use(express.json())
 // setting up express middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // setting up bodyParser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // express router middleware
-app.use("/api/signup/", routerSignup);
-app.use("/api/login/", routerLogin);
-app.use("/api/barang/", routerBarang);
-app.use("/api/logout/", routerLogout);
-app.use("/api/akun/", routerAkun);
-app.use("/api/jenis/", routerJenisBarang);
-app.use("/api/rekening/", routerRekening);
-app.use("/api/keranjang/", routerKeranjang);
-app.use("/api/pemesanan/", routerPemesanan);
+app.use('/api/signup', routerSignup)
+app.use('/api/login', routerLogin)
+app.use('/api/barang', routerBarang)
+app.use('/api/logout', routerLogout)
+app.use('/api/akun', routerAkun)
+app.use('/api/jenis', routerJenisBarang)
+app.use('/api/rekening', routerRekening)
+app.use('/api/keranjang', routerKeranjang)
+app.use('/api/pemesanan', routerPemesanan)
 
 const start = async () => {
-    try {
-        await sequelize.sync({ force: true });
-        await sequelize.sync({alter: true});
+  try {
+    await sequelize.sync({ force: true })
+    await sequelize.sync({ alter: true })
 
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
-        
-        await server.listen(port, () => {
-            console.log(`server is listening on http://localhost:${port}`);
-        });
-    }
-    catch (err) {
-        console.log(`failed to connect to database\n\n` + err);
-    }
-};
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
 
-start();
+    await server.listen(port, () => {
+      console.log(`server is listening on http://localhost:${port}`)
+    })
+  } catch (err) {
+    console.log(`failed to connect to database\n\n` + err)
+  }
+}
+
+start()
--- backend/controllers/controllerBarang.js
+++ backend/controllers/controllerBarang.js
@@ -1,269 +1,286 @@
-const { Barang, JenisBarang, FotoBarang } = require('../database/models');
-const { Op } = require("sequelize");
-const fs = require("fs");
+const {
+  Barang,
+  JenisBarang,
+  FotoBarang,
+} = require('../database/models')
+const { Op } = require('sequelize')
+const fs = require('fs')
 
 // for all
 const daftarBarang = async (req, res) => {
-    try {
-        const semuaBarang = await FotoBarang.findAll({
-            include: {
-                model: Barang,
-                required: true,
-                include: {
-                    model: JenisBarang,
-                    required: true // inner join
-                }
-            }
-        });
+  try {
+    const semuaBarang = await FotoBarang.findAll({
+      include: {
+        model: Barang,
+        required: true,
+        include: {
+          model: JenisBarang,
+          required: true, // inner join
+        },
+      },
+    })
 
-        res.status(200).json(semuaBarang).end();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: err }).end();
-    }
-};
+    res.status(200).json(semuaBarang).end()
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ msg: err }).end()
+  }
+}
 
 // admin only
 const tambahBarang = async (req, res) => {
-    const {
-        namaBarang,
-        merek,
-        berat,
-        jenis,
-        harga,
-        stok,
-        deskripsi
-    } = req.body;
+  const { namaBarang, merek, berat, jenis, harga, stok, deskripsi } =
+    req.body
 
-    let foto = req.file.path;
+  let foto = req.file.path
 
-    try {
-        const jenisId = await JenisBarang.findOne({
-            where: { nama: jenis }
-        });
+  try {
+    const jenisId = await JenisBarang.findOne({
+      where: { nama: jenis },
+    })
 
-        if (jenisId === null) {
-            throw 'Jenis barang tidak ditemukan!';
+    if (jenisId === null) {
+      throw 'Jenis barang tidak ditemukan!'
+    } else {
+      const barang = await FotoBarang.create(
+        {
+          foto: foto,
+          Barang: {
+            nama: namaBarang,
+            merek,
+            berat,
+            jenisId: jenisId.id,
+            harga,
+            stok,
+            deskripsi,
+          }, // masih error, harusnya b nya lowercase
+        },
+        {
+          include: Barang,
         }
-        
-        else {
-            
-            const barang = await FotoBarang.create({
-                foto: foto,
-                Barang: {
-                    nama: namaBarang,
-                    merek,
-                    berat,
-                    jenisId: jenisId.id,
-                    harga,
-                    stok,
-                    deskripsi
-                } // masih error, harusnya b nya lowercase
-            },
-            {
-                include: Barang
-            });
-            console.log(barang);  
-            res.status(200).json({
-                status: "Success",
-                msg: "Berhasil menambahkan barang!",
-                data: barang
-            }).end();
-        }
+      )
+      console.log(barang)
+      res
+        .status(200)
+        .json({
+          status: 'Success',
+          msg: 'Berhasil menambahkan barang!',
+          data: barang,
+        })
+        .end()
     }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: err }).end();
-    }
-
-    
-
-
-};
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ msg: err }).end()
+  }
+}
 
 // for all
 const detailBarang = async (req, res) => {
-    const id = req.query.id;
+  const { id } = req.params
 
-    try {
-        const infoBarang = await FotoBarang.findOne({
-            where: {
-                BarangId: id
-            },
-            include: {
-                model: Barang,
-                include: {
-                    model: JenisBarang,
-                    required: true, // create an inner join
-                }
-            }
-        });
-        res.status(200).json({
-            status: "success",
-            data: infoBarang
-        }).end();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            msg: err
-        }).end();
-    }
-};
+  try {
+    const infoBarang = await FotoBarang.findOne({
+      where: {
+        BarangId: id,
+      },
+      include: {
+        model: Barang,
+        include: {
+          model: JenisBarang,
+          required: true, // create an inner join
+        },
+      },
+    })
+    res
+      .status(200)
+      .json({
+        status: 'success',
+        data: infoBarang,
+      })
+      .end()
+  } catch (err) {
+    console.log(err)
+    res
+      .status(500)
+      .json({
+        msg: err,
+      })
+      .end()
+  }
+}
 
 // edit sudah benar tapi ragu (admin only)
 const ubahDataBarang = async (req, res) => {
-    const id = req.params.id;
-    const {
-        namaBarang,
+  const id = req.params.id
+  const { namaBarang, merek, berat, jenis, harga, stok, deskripsi } =
+    req.body
+
+  const img = req.file
+
+  try {
+    const fotoBarang = await FotoBarang.findOne({
+      where: { BarangId: id },
+    })
+
+    if (img) {
+      if (fotoBarang.foto) fs.unlinkSync(`${fotoBarang.foto}`)
+      const updateData = await fotoBarang.update(
+        { foto: req.file.path },
+        { where: { BarangId: id } }
+      )
+
+      res
+        .status(200)
+        .json({
+          status: 'success',
+          msg: 'Berhasil memperbarui barang!',
+          data: updateData,
+        })
+        .end()
+
+      return
+    }
+
+    const barang = await Barang.findOne({
+      where: {
+        id: id,
+      },
+    })
+
+    if (barang) {
+      const jenisId = await JenisBarang.findOne({
+        where: { nama: jenis },
+      })
+
+      if (jenisId === null) {
+        throw 'jenis tidak ditemukan!'
+      }
+      const newData = {
+        nama: namaBarang,
         merek,
         berat,
-        jenis,
+        jenisId: jenisId.id,
         harga,
         stok,
-        deskripsi
-    } = req.body;
+        deskripsi,
+      }
 
-    const img = req.file;
+      const updateBarang = await Barang.update(newData, {
+        where: {
+          id: id,
+        },
+      })
+      if (!updateBarang) throw 'Gagal mengubah data barang!'
 
-    try {
-        const fotoBarang = await FotoBarang.findOne({ where: { BarangId: id } });
+      res
+        .status(200)
+        .json({
+          status: 'success',
+          msg: 'Berhasil memperbarui barang!',
+          data: updateBarang,
+        })
+        .end()
 
-        if (img) {
-            if (fotoBarang.foto) fs.unlinkSync(`${fotoBarang.foto}`);
-            const updateData = await fotoBarang.update({ foto: req.file.path }, { where: { BarangId: id } });
-
-            res.status(200).json({
-                status: 'success',
-                msg: 'Berhasil memperbarui barang!',
-                data: updateData
-            }).end();
-
-            return;
-        }
-
-        const barang = await Barang.findOne({
-            where: {
-                id: id
-            }
-        });
-
-        if (barang) {
-            const jenisId = await JenisBarang.findOne({ where: { nama: jenis } });
-        
-            if (jenisId === null) {
-                throw 'jenis tidak ditemukan!';
-            }
-            const newData = {
-                nama: namaBarang,
-                merek,
-                berat,
-                jenisId: jenisId.id,
-                harga,
-                stok,
-                deskripsi
-            };
-
-            const updateBarang = await Barang.update(newData, {
-                where: {
-                    id: id
-                }
-            });
-            if (!updateBarang) throw 'Gagal mengubah data barang!';
-
-            res.status(200).json({
-                status: 'success',
-                msg: 'Berhasil memperbarui barang!',
-                data: updateBarang
-            }).end();
-
-            return;
-        }
-        else throw 'Gagal memperbarui barang!';
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            status: "failed",
-            msg: [err]
-        }).end();
-    }
-};
+      return
+    } else throw 'Gagal memperbarui barang!'
+  } catch (err) {
+    console.log(err)
+    res
+      .status(500)
+      .json({
+        status: 'failed',
+        msg: [err],
+      })
+      .end()
+  }
+}
 
 const hapusBarang = async (req, res) => {
-    let { id } = req.params;
+  let { id } = req.params
 
-    try {
-        const barang = await Barang.findOne({
-            where: {
-                id: id
-            }
-        });
+  try {
+    const barang = await Barang.findOne({
+      where: {
+        id: id,
+      },
+    })
 
-        if (!barang) {
-            throw 'Gagal menghapus barang!';
-        }
-        else {
-            const fotoBarang = await FotoBarang.findOne({
-                where: {
-                    BarangId: id
-                }
-            });
-            
-            // delete photo from local storage
-            fs.unlink(`${fotoBarang.foto}`, (err) => {
-                if (err) throw 'Gagal menghapus foto dari penyimpanan lokal!';
-                else console.log('Berhasil menghapus foto dari penyimpanan lokal!');
-            });
-            // delete photo's path from database
-            await barang.destroy();
+    if (!barang) {
+      throw 'Gagal menghapus barang!'
+    } else {
+      const fotoBarang = await FotoBarang.findOne({
+        where: {
+          BarangId: id,
+        },
+      })
 
-            res.status(200).json({
-                status: 'success',
-                msg: 'Barang berhasil dihapus!'
-            }).end();
-        }
+      // delete photo from local storage
+      fs.unlink(`${fotoBarang.foto}`, (err) => {
+        if (err) throw 'Gagal menghapus foto dari penyimpanan lokal!'
+        else
+          console.log(
+            'Berhasil menghapus foto dari penyimpanan lokal!'
+          )
+      })
+      // delete photo's path from database
+      await barang.destroy()
+
+      res
+        .status(200)
+        .json({
+          status: 'success',
+          msg: 'Barang berhasil dihapus!',
+        })
+        .end()
     }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            status: 'fail',
-            msg: [err]
-        }).end();
-    }
-};
+  } catch (err) {
+    console.log(err)
+    res
+      .status(500)
+      .json({
+        status: 'fail',
+        msg: [err],
+      })
+      .end()
+  }
+}
 
 const cariBarang = async (req, res) => {
-    // const input = req.body.input;
-    const nama = req.query.nama;
+  // const input = req.body.input;
+  const nama = req.query.nama
 
-    try {
-        const foundBarang = await Barang.findAll({
-            where: {
-                nama: { [Op.iLike]: `%${nama}%` }
-             }
-        });
+  try {
+    const foundBarang = await Barang.findAll({
+      where: {
+        nama: { [Op.iLike]: `%${nama}%` },
+      },
+    })
 
-        console.log(nama);
-        res.status(201).json({
-            status: 'success',
-            data: foundBarang
-        }).end();
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            msg: [err]
-        }).end();
-    }
-};
+    console.log(nama)
+    res
+      .status(201)
+      .json({
+        status: 'success',
+        data: foundBarang,
+      })
+      .end()
+  } catch (err) {
+    console.log(err)
+    res
+      .status(500)
+      .json({
+        msg: [err],
+      })
+      .end()
+  }
+}
 
 module.exports = {
-    daftarBarang,
-    tambahBarang,
-    detailBarang,
-    ubahDataBarang,
-    hapusBarang,
-    cariBarang
-};
+  daftarBarang,
+  tambahBarang,
+  detailBarang,
+  ubahDataBarang,
+  hapusBarang,
+  cariBarang,
+}
