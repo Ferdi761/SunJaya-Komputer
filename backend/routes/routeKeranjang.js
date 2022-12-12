f760d3732e24ecc8b784068aa522264576eed439
Ferytom <garuda.bangkit@gmail.com>
2022-12-12 15:12:30 +0700

    add post

--- backend/routes/routeKeranjang.js
+++ backend/routes/routeKeranjang.js
@@ -1,16 +1,19 @@
-const express = require("express");
-const routerKeranjang = express.Router();
+const express = require('express')
+const routerKeranjang = express.Router()
 
-const loginCheck = require('../middlewares/loginCheck');
+const loginCheck = require('../middlewares/loginCheck')
 const {
-    daftarKeranjang,
-    hapusDariKeranjang,
-    allCartList
-} = require("../controllers/controllerKeranjang");
+  daftarKeranjang,
+  hapusDariKeranjang,
+  tambahKeKeranjang,
+  allCartList,
+} = require('../controllers/controllerKeranjang')
 
-routerKeranjang.route("/")
-.get(loginCheck, daftarKeranjang)
-.delete(loginCheck, hapusDariKeranjang);
-routerKeranjang.route("/allCart").get(allCartList);
+routerKeranjang
+  .route('/')
+  .get(loginCheck, daftarKeranjang)
+  .post(loginCheck, tambahKeKeranjang)
+  .delete(loginCheck, hapusDariKeranjang)
+routerKeranjang.route('/allCart').get(allCartList)
 
-module.exports = routerKeranjang;
+module.exports = routerKeranjang
